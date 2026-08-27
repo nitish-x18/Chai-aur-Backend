2c5f44a62ee5382d8997b21c8530fb15c0120773
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-27 12:40:38 +0530

    fix: improve formatting and consistency in verifyJWT middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,19 +6,19 @@ import jwt from "jsonwebtoken"
 export const verifyJWT = asyncHandler(async (req, res, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-    
+
         if (!token) {
             throw new apiError(401, "unauthorized request")
         }
-    
+
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-    
+
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
-    
-        if(!user) {
+
+        if (!user) {
             throw new apiError(401, "Invalid Access Token")
         }
-    
+
         req.user = user;
         next()
 
