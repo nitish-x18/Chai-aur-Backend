338348c6e0170f3aba878c46528af6160b40a35c
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-13 20:52:44 +0530

    fix: correct video route version from v2 to v1

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/v1/users", userRouter);
-app.use("/api/v2/vedio", vedioRouter);
+app.use("/api/v1/vedio", vedioRouter);
 
 console.log("USER ROUTER LOADED");
 
