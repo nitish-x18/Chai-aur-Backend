9d5d220f410fca3394ce7ab0e1cd18e6cf5000e5
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-17 22:27:06 +0530

    feat: add tweet router to app.js for tweet-related API endpoints

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ app.use(cookieParser());
 //routes import-->
 import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
 
 
 //router declaration-->
@@ -27,6 +28,7 @@ app.use((req, res, next) => {
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/tweet", tweetRouter);
 
 console.log("USER ROUTER LOADED");
 
