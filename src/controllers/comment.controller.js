50c0ce616476db90a3153de009e935e9330f3c27
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-20 10:26:58 +0530

    style: format code for consistency in getVideoComments function

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,26 +1,26 @@
 import mongoose, { isValidObjectId } from "mongoose"
-import {Comment} from "../models/comment.model.js"
-import {apiError} from "../utils/apiError.js"
-import {apiResponse} from "../utils/apiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Comment } from "../models/comment.model.js"
+import { apiError } from "../utils/apiError.js"
+import { apiResponse } from "../utils/apiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+    const { videoId } = req.params
+    const { page = 1, limit = 10 } = req.query
 
-    if(!videoId){
+    if (!videoId) {
         throw new apiError(400, "video id is required")
     }
 
-    if(!isValidObjectId(videoId)){
+    if (!isValidObjectId(videoId)) {
         throw new apiError(404, "video is not found")
     }
 
     const pageNumber = Number(page)
     const limitNumber = Number(limit)
 
-    if(pageNumber < 1 || limitNumber < 1){
+    if (pageNumber < 1 || limitNumber < 1) {
         throw new apiError(400, "page and li it must be greater than 0")
     }
 
@@ -31,18 +31,18 @@ const getVideoComments = asyncHandler(async (req, res) => {
             video: videoId,
         }
     )
-    .skip(skip)
-    .limit(limitNumber)
+        .skip(skip)
+        .limit(limitNumber)
 
-    if(comments.length === 0){
+    if (comments.length === 0) {
         throw new apiError(404, "commnet not found")
     }
 
     return res
-    .status(200)
-    .json(
-        new apiResponse(200, comments, "succesfully getting all comments")
-    )
+        .status(200)
+        .json(
+            new apiResponse(200, comments, "succesfully getting all comments")
+        )
 
 })
 
@@ -59,8 +59,8 @@ const deleteComment = asyncHandler(async (req, res) => {
 })
 
 export {
-    getVideoComments, 
-    addComment, 
+    getVideoComments,
+    addComment,
     updateComment,
-     deleteComment
-    }
+    deleteComment
+}
