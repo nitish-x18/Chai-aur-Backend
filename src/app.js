87a7fc2ca3ccb46ee43d31fb8bd35d2fabc108e3
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-21 21:55:19 +0530

    fix: correct typo in commentRouter import and add playlistRouter

--- src/app.js
+++ src/app.js
@@ -18,7 +18,8 @@ app.use(cookieParser());
 import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
-import commnetRouter from "./routes/comment.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
 
 
 //router declaration-->
@@ -30,7 +31,8 @@ app.use((req, res, next) => {
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/video", videoRouter);
 app.use("/api/v1/tweet", tweetRouter);
-app.use("/api/v1/comment", commnetRouter);
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/playlist", playlistRouter);
 
 console.log("USER ROUTER LOADED");
 
