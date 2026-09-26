733854e9a1b4e0d1a6fd36679c9051667d5b4d4a
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-26 19:22:19 +0530

    fix: correct import path for subscription router in app.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ import tweetRouter from "./routes/tweet.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import likeRouter from "./routes/like.routes.js";
-import subscriptionRoute from "./models/subscription.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 
 //router declaration-->
@@ -36,7 +36,7 @@ app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/likes", likeRouter);
-app.use("/api/v1/subscriptions", subscriptionRoute)
+app.use("/api/v1/subscriptions", subscriptionRouter)
 
 console.log("USER ROUTER LOADED");
 
