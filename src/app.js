4d8fb713c08272f67d820f5ca35a9413148be9b1
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-26 19:11:08 +0530

    feat: add subscription route to the API

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ import tweetRouter from "./routes/tweet.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import likeRouter from "./routes/like.routes.js";
+import subscriptionRoute from "./models/subscription.routes.js";
 
 
 //router declaration-->
@@ -35,6 +36,7 @@ app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/subscriptions", subscriptionRoute)
 
 console.log("USER ROUTER LOADED");
 
